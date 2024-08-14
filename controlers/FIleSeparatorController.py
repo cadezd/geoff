ba0e211e63ca8f4e75be016e242fbd85eddd32b1
David Čadež <david.cadez89@gmail.com>
2024-08-14 15:09:34 +0200

    Added functionality for loading, saving and setting the settings.

--- controlers/FIleSeparatorController.py
+++ controlers/FIleSeparatorController.py
@@ -1,9 +1,53 @@
-class FileSeparatorController:
-    def __init__(self):
-        self.documents: list[str] = []
-        self.grouped_documents: dict[str: list[str]] = {}
+from __future__ import annotations
 
-        self.settings = {
+import os
+import pickle
+
+from utils.Util import is_regex_valid
+
+
+class FileSeparatorController:
+    def __init__(self, settings_path: str = os.path.join("..", "separator_settings", "settings.pkl")):
+        # Settings path
+        self.settings_path: str = settings_path
+        # Default settings
+        self.settings: dict[str, int | float | str] = self._load_settings()
+        # Valid values for each setting
+        self.valid_settings_values: dict[str, list[int | float]] = {
+            "DPI_IN": [200, 250, 300, 350, 400],
+            "DPI_OUT": [140],
+            "ZOOM": [1, 2, 3],
+            "MASKA": [150, 175, 200, 225, 250],
+            "PODROCJE": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            "BELOST": [240, 245, 250, 255],
+            "DEFAULT_SEPARATE": [0, 1],
+        }
+
+        self.grouped_documents: dict[str, list[str]] = {
+            "Document 1": [
+                os.path.join("..", "assets", "image0.png"),
+                os.path.join("..", "assets", "image1.jpg"),
+                os.path.join("..", "assets", "image2.jpg"),
+            ],
+            "Document 2": [
+                os.path.join("..", "assets", "image0.png"),
+                os.path.join("..", "assets", "image1.jpg"),
+                os.path.join("..", "assets", "image2.jpg"),
+            ],
+            "Document 3": [
+                os.path.join("..", "assets", "image0.png"),
+                os.path.join("..", "assets", "image1.jpg"),
+                os.path.join("..", "assets", "image2.jpg"),
+            ],
+            "Document 4": [
+                os.path.join("..", "assets", "image0.png"),
+                os.path.join("..", "assets", "image1.jpg"),
+                os.path.join("..", "assets", "image2.jpg"),
+            ],
+        }
+
+    def _load_settings(self) -> dict:
+        settings: dict[str, int | float | str] = {
             "DPI_IN": 300,
             "DPI_OUT": 140,
             "ZOOM": 2,
@@ -11,47 +55,33 @@ class FileSeparatorController:
             "PODROCJE": 0.3,
             "BELOST": 255,
             "FILTER": r"(DPR|CE|DDB|DOV|PRO|PADNI|DNI|NRA|PDO|NPR|PRA)([0-9]{2})(\+|-|_)([0-9]{4})",
-            "DEFAULT_SEPARATE": "QR koda",
+            "DEFAULT_SEPARATE": 0,
         }
 
-    def set_documents(self, documents: list[str]):
-        ...
+        # Creates settings file if it doesn't exist
+        if not os.path.exists(self.settings_path):
+            os.mkdir(os.path.dirname(self.settings_path))
+            with open(self.settings_path, 'wb') as f:
+                pickle.dump(settings, f)
 
-    def group_documents(self):
-        ...
+        # Loads data from file
+        with open(self.settings_path, 'rb') as f:
+            settings = pickle.load(f)
 
-    def rename_document(self, old_name: str, new_name: str):
-        ...
+        return settings
 
-    def rearrange_documents(self, idx_from: int, idx_to: int):
-        ...
+    def save_settings(self) -> None:
+        # Save settings to file
+        with open(self.settings_path, 'wb') as f:
+            pickle.dump(self.settings, f)
 
-    def delete_document_pages(self, document_name: str, indexes: list[int]):
-        ...
+    def set_setting(self, key: str, value: int | float | str) -> None:
+        if key in self.valid_settings_values:
+            if value in self.valid_settings_values[key]:
+                self.settings[key] = value
+        elif key == "FILTER":
+            if is_regex_valid(value.strip()):
+                self.settings[key] = value.strip()
 
-    def delete_document(self, document_name: str):
-        ...
 
-    def create_new_document(self, document_name: str, pages: list[str]):
-        ...
-
-    def clear_grouped_documents(self):
-        ...
-
-    def clear_documents(self):
-        ...
-
-    def load_settings(self):
-        ...
-
-    def save_settings(self):
-        ...
-
-    def get_setting(self, key):
-        ...
-
-    def set_setting(self, key, value):
-        ...
-
-    def save_grouped_documents(self):
-        ...
+file_separator_controller = FileSeparatorController()
