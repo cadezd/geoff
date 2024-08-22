e2e19d1b246aede26670fc4b9de372af40f4063f
David Čadež <david.cadez89@gmail.com>
2024-08-22 09:01:57 +0200

    Finished the home view. Added welcome text, brief description of the app and simple animation of Geoff the car.

--- views/HomeView.py
+++ views/HomeView.py
@@ -1,7 +1,8 @@
 import os.path
+from math import pi
 
 import flet as ft
-from flet import Column, Image, Row
+from flet import Column, Row, Container, Image, Text, TextSpan, ElevatedButton
 
 
 class HomeView(Column):
@@ -10,13 +11,65 @@ class HomeView(Column):
         self.page = page
         self.alignment = ft.MainAxisAlignment.CENTER
         self.expand = True
-        self.page.on_keyboard_event = None
+
+        self.counter = 0
+
+        # Geoff (the car) that will be animated
+        self.geoff: Container = Container(
+            content=Image(
+                src=os.path.join("..", "assets", "geoff.png"),
+            ),
+            offset=ft.transform.Offset(5, 0),
+            animate_offset=ft.animation.Animation(1000),
+            rotate=ft.transform.Rotate(0, alignment=ft.alignment.center),
+            animate_rotation=ft.animation.Animation(1000, ft.AnimationCurve.BOUNCE_OUT),
+        )
+
+        # Controls of the HomeView
         self.controls = [
             Row(
                 controls=[
-                    Image(src=os.path.join("assets", "home_view_image.png"), width=500, height=500, ),
+                    Text("👋Dobrodošli nazaj!", size=36),
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
-                expand=True
-            )
+            ),
+            Row(
+                controls=[
+                    Text(spans=[
+                        TextSpan("GEOFF", url="https://topgear.fandom.com/wiki/Geoff"),
+                        TextSpan(" je namenska aplikacija za obdelavo dokumentov."),
+                    ], size=24),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            Row(
+                controls=[
+                    self.geoff,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            Row(
+                controls=[
+                    ElevatedButton("Kje je Geoff?", on_click=self.animate, style=ft.ButtonStyle(
+                        padding=20
+                    )),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
         ]
+
+    def animate(self, e):
+        """
+        Animates Geoff (the car)
+        :param e:
+        :return:
+        """
+        if self.counter % 3 == 0:
+            self.geoff.offset = ft.transform.Offset(0, 0)
+        elif self.counter % 3 == 1:
+            self.geoff.rotate.angle += pi * 2
+        elif self.counter % 3 == 2:
+            self.geoff.offset = ft.transform.Offset(5, 0)
+
+        self.page.update()
+        self.counter += 1
